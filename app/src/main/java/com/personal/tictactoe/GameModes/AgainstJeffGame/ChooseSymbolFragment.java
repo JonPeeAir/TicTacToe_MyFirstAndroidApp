90e618d4afa356584cffe58500d93c322caff786
John Pierre Mendoza <johnpierremendoza@gmail.com>
2021-05-26 13:55:27 +0800

    ChooseSymbolFragment layout edited

---
+++ app/src/main/java/com/personal/tictactoe/GameModes/AgainstJeffGame/ChooseSymbolFragment.java
@@ -0,0 +1,41 @@
+package com.personal.tictactoe.GameModes.AgainstJeffGame;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.personal.tictactoe.R;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ChooseSymbolFragment extends Fragment {
+
+    Button xButton;
+    Button oButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_choose_symbol, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        xButton = view.findViewById(R.id.choice_x_button);
+        oButton = view.findViewById(R.id.choice_o_button);
+
+
+
+
+    }
+}
