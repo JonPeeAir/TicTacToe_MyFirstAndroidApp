becdbcbb397f8226f5e88fd6b3566f39d456a204
John Pierre Mendoza <johnpierremendoza@gmail.com>
2021-05-26 14:27:25 +0800

    GameUtils initially created

---
+++ app/src/main/java/com/personal/tictactoe/GameModes/AgainstJeffGame/GameUtils.java
@@ -0,0 +1,46 @@
+package com.personal.tictactoe.GameModes.AgainstJeffGame;
+
+import com.personal.tictactoe.R;
+
+public class GameUtils {
+
+    String X;
+    String O;
+    String EMPTY;
+
+    public GameUtils() {
+        X = "X";
+        O = "O";
+        EMPTY = "";
+    }
+
+    public String player(String[][] board) {
+        return null;
+    }
+
+    public int[] actions(String[][] board) {
+        return null;
+    }
+
+    public String[][] results(String[][] board, int[] action) {
+        return null;
+    }
+
+    public String winner(String[][] board) {
+        return null;
+    }
+
+    public boolean terminal(String[][] board) {
+        return false;
+    }
+
+    public int utility(String[][] board) {
+        return 0;
+    }
+
+    public int[] minimax(String[][] board) {
+        return null;
+    }
+
+
+}
